53b37e62a290fc0c58b3c560d77d88f1a22c14bb
Yl727 <yoineralexis.lopezquintana727@gmail.com>
2020-11-16 09:06:37 -0500

    AminLTE e inicio de publicaciones

---
+++ app/Models/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title','content','precio','estado','facultad_id'];
+    /**
+     * Falta realizar la relación con facultades
+     */
+    
+}
