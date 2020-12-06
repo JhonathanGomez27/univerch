08a0afe8da51c4673f94c50d6a9fa6e934453d0c
jhonEdison1 <47945021+jhonEdison1@users.noreply.github.com>
2020-12-06 15:41:12 -0500

    Sentencia corregida

---
+++ resources/views/paypal/success.blade.php
@@ -0,0 +1,11 @@
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+             holi
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
