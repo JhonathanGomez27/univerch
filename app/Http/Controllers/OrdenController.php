74e193dfa87521d2194f90e69f9936f9f9e4669e
JhonathanGomez27 <jhonathan.gomezm@autonoma.edu.co>
2020-12-06 22:58:18 -0500

    vista ventas y compras

--- app/Http/Controllers/OrdenController.php
+++ app/Http/Controllers/OrdenController.php
@@ -15,18 +15,21 @@ class OrdenController extends Controller
         ->where('estado', '=', 'aprovado')
         ->where('comprador_id','=',$id)
         ->paginate(10);
-        return view('dashboard.orders.index',['orders'=>$orders]);
-    }
 
-
-    public function getMySales()
-    {
-        $id = auth()->id();
         //Para filtar por id//->where('id', '=', '2')->paginate(10);
         $sales = Order::orderBy('created_at','desc')
         ->where('estado', '=', 'aprovado')
         ->where('vendedor_id','=',$id)
+
+        ->paginate(10);
+        return view('dashboard.orders.index',['orders'=>$orders ,'sales'=>$sales]);
+    }
+
+
+    /* public function getMySales()
+    {
+
         ->paginate(10);
         return view('dashboard.orders.index',['sales'=>$sales]);
-    }
+    } */
 }
--- resources/views/dashboard/orders/index.blade.php
+++ resources/views/dashboard/orders/index.blade.php
@@ -1,42 +1,71 @@
 @extends('dashboard.master')
 @section('content')
 
-<div class="row sm-3">
-  <div class="col-4">
-    <div >
-        <div class>
-            <h5 class>Mis compras</h5>
+<div class="container-sales-orders">
+    <div class="container-so">
+        <div class="row ">
+            <div class="col sm-2">
+                <h1 class='retroshadow'>Mis Ventas</h1>
+                <br>
+              <table class="table table-striped">
+                  <thead>
+                      <th>Id</th>
+                      <th>Producto</th>
+                      <th>Comprador</th>
+                      <th>Vendedor</th>
+                  </thead>
+                      <tbody>
 
+                              @foreach ($sales as $sale)
+                                <tr>
+                                  <td>{{ $sales}}</td>
+                                  <td>{{ $sale->product->title  }}</td>
+                                  <td>{{ $sale->comprador->name  }}</td>
+                                  <td>{{ $sale->vendedor->name}}</td>
+                                </tr>
+                              @endforeach
 
-        </div>
-      </div>
-  </div>
-  <div class="col-2">
-    <table class="table table-striped">
-        <thead>
-            <th>Id</th>
-            <th>Producto</th>
-            <th>Comprador</th>
-            <th>Vendedor</th>
-        </thead>
-            <tbody>
+                       </tbody>
+              </table>
 
-                    @foreach ($sales as $sale)
-                      <tr>
-                        <td>{{ $sale->id }}</td>
-                        <td>{{ $sale->product->title  }}</td>
-                        <td>{{ $sale->comprador->name  }}</td>
-                        <td>{{ $sale->vendedor->name}}</td>
-                      </tr>
-                    @endforeach
+              <div class="btn--archivos">
+                <input class="btn pdf--btn" value="Generar PDF">
+                <input class="btn pdf--btn" style="visibility: hidden;" value="">
+                <input class="btn excel--btn" value="Generar Excel">
+              </div>
 
-             </tbody>
+              <br>
+              <div class="col sm-2">
+                <h1 class='retroshadow'>Mis Compras</h1>
+                <br>
+                    <table class="table table-striped">
+                        <thead>
+                            <th>Id</th>
+                            <th>Producto</th>
+                            <th>Comprador</th>
+                            <th>Vendedor</th>
+                        </thead>
+                            <tbody>
+                                @foreach ($orders as $order)
+                                    <tr>
+                                        <td>{{ $order->id}}</td>
+                                        <td>{{ $order->product->title}}</td>
+                                        <td>{{ $order->comprador->name}}</td>
+                                        <td>{{ $order->vendedor->name}}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                    </table>
+                </div>
+                <div class="btn--archivos">
+                    <input class="btn pdf--btn" value="Generar PDF">
+                    <input class="btn pdf--btn" style="visibility: hidden;" value="">
+                    <input class="btn excel--btn" value="Generar Excel">
+                  </div>
+              </div>
+          </div>
+    </div>
 
 </div>
-</div>
-
-
-
-
 
 @endsection
