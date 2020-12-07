db9aba01fd57a68d27407c13531e8720d9d5ad4b
JhonathanGomez27 <jhonathan.gomezm@autonoma.edu.co>
2020-12-06 23:44:03 -0500

    vista facultados y botones flotantes

--- resources/views/dashboard/facultad/show.blade.php
+++ resources/views/dashboard/facultad/show.blade.php
@@ -1,13 +1,25 @@
-<div class="card">
+<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">{{$facultad->title}}</h5>
+      <p class="card-text">{{ $facultad->content }}</p>
+      <a class="card-link btn btn-outline-primary" href="{{route('facultad.edit',$facultad->id)}}" class="btn btn-outline-primary">Editar</a>
+        <button  data-toggle="modal" data-target="#deleteModal" data-id="{{$facultad->id}}" class="btn btn-outline-danger card-link">
+            Eliminar
+        </button>
+    </div>
+  </div>
+
+{{-- <div class="card">
     <div class="card-body">
         <h5 class="card-title">{{$facultad->title}}</h5>
-        <h6 class="card-text">{{ $facultad->content }}</h6>  
+        <h6 class="card-text">{{ $facultad->content }}</h6>
         <a href="{{route('facultad.edit',$facultad->id)}}" class="btn btn-outline-primary">Editar</a>
         <button data-toggle="modal" data-target="#deleteModal" data-id="{{$facultad->id}}" class="btn btn-outline-danger">
             Eliminar
-        </button>    
+        </button>
     </div>
 </div>
+ --}}
  <!-- Modal -->
  <div class="modal fade" id="deleteModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
